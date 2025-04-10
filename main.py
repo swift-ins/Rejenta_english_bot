8fbfbd374aab7ef1e45caed25a85ccee095499ff
АСУС <natalja.rejenta@gmail.com>
2025-04-10 15:20:16 +0300

    add

--- main.py
+++ main.py
@@ -80,4 +80,4 @@ if __name__ == '__main__':
         print("Вебхук установлен")
 
     asyncio.run(setup())
-    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000)))
+    app.run(host='0.0.0.0', port=int(os.environ.get('PORT', 5000))) 
